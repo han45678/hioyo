53e6069fa9dc453ef4d5dfa16639a46a194bfd42
HAN <han0910812144@gmail.com>
2020-08-27 18:52:12 +0800

    2020-08-27 6:52

---
+++ product/index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../includes/head_content.php' ?>
+</head>
+
+<body id="category">
+    <?php include '../includes/topBar_content.php' ?>
+    <?php include '../includes/header_content.php' ?>
+    <div id="top_category_menu">
+        <div class="container">
+            <div class="row">
+                <div id="category_order" class="col-lg-12 col-md-12">
+                    <div id="main_category">
+                        <a href="">所有商品</a>
+                        <a href="">尊爵套組</a>
+                        <a href="">抗疫專區</a>
+                        <a href="">利奇</a>
+                        <a href="">皇家維多利亞</a>
+                        <a href="">活動專區</a>
+                    </div>
+                </div>
+                <ul>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                    <li><a href="../sub_category">商品類別</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <?php include '../includes/menu_content.php' ?>
+                    <div id="category_menu">
+                        <div class="title">
+                            商品類別
+                        </div>
+                        <div class="content">
+                            <ul>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                                <li><a href="../sub_category">商品小類別</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="leaderboard">
+                        <div class="title">
+                            暢銷排行榜
+                        </div>
+                        <ul>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <div class="photo">
+                                        <img src="../image/p1.jpg" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <h3>
+                                            ABCDEFGHIJK
+                                        </h3>
+                                        <span class="price">1999</span>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="category_order" class="col-lg-10 col-md-12">
+                    <div id="path">
+                        <ol>
+                            <li><a href="../">Home</a></li>
+                            <li><a href="../main_category">商品類別</a></li>
+                            <li><a href="../sub_category">商品小類別</a></li>
+                            <li><a href="../product">商品名稱</a></li>
+                        </ol>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php include '../includes/footer_content.php' ?>
+    <?php include '../includes/footer_content_js.php' ?>
+</body>
+
+</html>
